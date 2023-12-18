2ec3abd8769aeefbe24faa9c6c5d736cdcae8ef7
Socrates <74465738+ramenwithparmesancheese@users.noreply.github.com>
2023-12-18 11:41:18 -0700

    Add files via upload

---
+++ main.py
@@ -0,0 +1,22 @@
+i = 1
+fizz = input("Pick any word.\n")
+buzz = input("Pick another word.\n")
+fizzbuzz = input("Last but not least, pick another word.\n")
+
+while i < 101:    
+    if i % 3 == 0 and i % 5 == 0:
+        print(fizz + buzz)
+
+
+    elif i % 3 == 0:
+        print(fizz)
+    
+    elif i % 5 == 0:
+        print(buzz)
+
+    else:
+        print(i)
+    
+
+    
+    i += 1
